9d1e7ae371bfea9182f5386c9fef4558ab6dcb93
DurianDan <huyk25sinh@gmail.com>
2023-02-06 10:50:59 +0700

    Initialize repo

---
+++ scrape_shopify/target/debug/deps/scrape_shopify-d7e6195f2eb9b369.d
@@ -0,0 +1,5 @@
+/home/new-user/project/Naree/scrape_shopify/target/debug/deps/scrape_shopify-d7e6195f2eb9b369.rmeta: src/main.rs
+
+/home/new-user/project/Naree/scrape_shopify/target/debug/deps/scrape_shopify-d7e6195f2eb9b369.d: src/main.rs
+
+src/main.rs:
