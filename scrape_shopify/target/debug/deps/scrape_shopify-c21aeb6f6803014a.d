a52d040c12cc5aa5014f3df91f28e3097a4101ee
Huy Nguyen Vu <huyk25sinh@gmail.com>
2023-09-18 23:57:30 +0700

    New structure. Aim big! You are strong because a strong woman raise you!

--- scrape_shopify/target/debug/deps/scrape_shopify-c21aeb6f6803014a.d
+++
@@ -1,5 +0,0 @@
-/home/new-user/project/Naree/scrape_shopify/target/debug/deps/scrape_shopify-c21aeb6f6803014a.rmeta: src/main.rs
-
-/home/new-user/project/Naree/scrape_shopify/target/debug/deps/scrape_shopify-c21aeb6f6803014a.d: src/main.rs
-
-src/main.rs:
