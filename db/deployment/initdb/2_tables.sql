043e280f47c6a5b7c7bd64ba64f9997e3bd209e3
Huy Nguyen Vu <huyk25sinh@gmail.com>
2023-09-22 23:26:19 +0700

    update: typo fixed from 'created_at' to 'created_on'

--- db/deployment/initdb/2_tables.sql
+++ db/deployment/initdb/2_tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE scraped.shopify_provider (
     shopify_page TEXT NOT NULL,
     apps_published INTEGER,
     avg_rating NUMERIC(3,1),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
@@ -16,7 +16,7 @@ CREATE TABLE scraped.shopify_app_category (
     parent_category_id INT,
     shopify_page TEXT NOT NULL,
     category_type VARCHAR(50),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     FOREIGN KEY (parent_category_id) REFERENCES scraped.shopify_app_category(id)
 );
@@ -29,7 +29,7 @@ CREATE TABLE scraped.shopfy_app_info (
     review_count INTEGER,
     provider_id INT,
     category_id INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     FOREIGN KEY (provider_id) REFERENCES scraped.shopify_provider(id),
     FOREIGN KEY (category_id) REFERENCES scraped.shopify_app_category(id)
@@ -46,7 +46,7 @@ CREATE TABLE scraped.shopify_app_reviews (
     days_spend_on_app FLOAT NOT NULL,
     rating INT NOT NULL,
     review_date TIMESTAMP NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     FOREIGN KEY (app_id) REFERENCES scraped.shopfy_app_info(id)
 );
 
@@ -57,7 +57,7 @@ CREATE TABLE scraped.shopify_pricing_plan (
     price VARCHAR(255),
     offer TEXT,
     app_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     FOREIGN KEY (app_id) REFERENCES scraped.shopfy_app_info(id)
 );
 
@@ -67,7 +67,7 @@ CREATE TABLE scraped.shopify_category_rank_log (
     category_id INT NOT NULL,
     "rank" INT NOT NULL,
     app_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     FOREIGN KEY (category_id) REFERENCES scraped.shopify_app_category(id),
     FOREIGN KEY (app_id) REFERENCES scraped.shopfy_app_info(id)
 );
@@ -77,6 +77,6 @@ CREATE TABLE scraped.shopify_app_description (
     id SERIAL PRIMARY KEY,
     app_id INT NOT NULL,
     description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     FOREIGN KEY (app_id) REFERENCES scraped.shopfy_app_info(id)
 );
