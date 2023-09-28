0b3144c54f15a6aa28a59ee400bd83659d7cb12b
Huy Nguyen Vu <huyk25sinh@gmail.com>
2023-09-28 12:27:02 +0700

    update: add function to compare 2 based scraped row data

--- scrape/scraper_ts/src/schemas/base_scraped_table.ts
+++ scrape/scraper_ts/src/schemas/base_scraped_table.ts
@@ -1,3 +1,5 @@
+const ld_diff = require('lodash.difference');
+
 interface BaseScrapedTable{
     id: Number,
     createdOn: Date|null,
@@ -8,11 +10,20 @@ function checkEqFields(
     row: BaseScrapedTable,
     otherRow: BaseScrapedTable
 ): void{
-    for  
+    for (const rowPair of [[row, otherRow],[otherRow, row]]){
+        const unmatchedFields: Array<string>= ld_diff.difference(
+            Object.keys(rowPair[0]),
+            Object.keys(rowPair[1])
+        );
+        if (unmatchedFields.length > 0){
+            throw "unmatched fields: " + unmatchedFields
+        }
+    }
 }
 
-function implements (row: BaseScrapedTable, otherRow: BaseScrapedTable): boolean{
-    return Object.keys(row).every((field)=>{
-        otherRow[field] === row[field]
+function isEqual (row: BaseScrapedTable, otherRow: BaseScrapedTable): boolean{
+    checkEqFields(row, otherRow);
+    return row._eqFields.every((field): boolean => {
+        return ((row as any)[field] === (row as any)[field])
     })
 }
