1d0c9a56f3dd578b3fcb204c2f4d74f7fbae19bb
Huy Nguyen Vu <huyk25sinh@gmail.com>
2023-10-05 00:28:13 +0700

    manage: update todo

---
+++ scrape/scraper_ts/src/ThePuppetShow/PuppetTricks.ts
@@ -0,0 +1,3 @@
+/**@todo Implements page scraping for individual Shopify URL.
+ * 1. Sitemap
+ * 2. Nested Categories in Advertising Categories*/
---
+++ scrape/scraper_ts/src/TheSalesman/config/index.ts
@@ -0,0 +1,1 @@
+/**@todo Create Specific ScrapeConfig for each website based on URL config and Elements Config */
