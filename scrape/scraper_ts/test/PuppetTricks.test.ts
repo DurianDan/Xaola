21d0e8688eb6ec4161464ec4c544cf70635444f2
Huy Nguyen Vu <huyk25sinh@gmail.com>
2023-10-08 23:51:29 +0700

    update: building testing scripts, with "Jest"

---
+++ scrape/scraper_ts/test/PuppetTricks.test.ts
@@ -0,0 +1,7 @@
+import initPuppet from "../src/initPupper";
+import { SitemapTrick } from "../src/ThePuppetShow/PuppetTricks";
+import { defaultLaunchOptions } from "../src/TheSalesman/config/browser";
+
+test('Check Init Browser', () => {
+    
+})
