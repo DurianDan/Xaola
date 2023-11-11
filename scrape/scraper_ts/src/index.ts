230870cfb3fba7ae44cf94eff2ff7db56e12febc
Huy Nguyen Vu <huyk25sinh@gmail.com>
2023-11-11 23:38:36 +0700

    update: successfully test FancyCategoryTrick to get the correct elements; tofix: excess app urls with parameters, sometimes TimeoutError because of loading elements

--- scrape/scraper_ts/src/index.ts
+++ scrape/scraper_ts/src/index.ts
@@ -1,15 +1,18 @@
 import PuppetMaster from './ThePuppetShow/PuppetMaster';
 // import SitemapTrick from './ThePuppetShow/PuppetTricks/SitemapTrick';
 import initPuppet from './initPuppet';
-import { defaultLaunchOptions } from './TheSalesman/config/browser';
+import {
+    debugLaunchOptions,
+    // defaultLaunchOptions
+} from './TheSalesman/config/browser';
 import { ConsoleWatcher } from './TheWatcher';
-import AppLandingPageTrick from './ThePuppetShow/PuppetTricks/AppLandingPageTrick';
+// import AppLandingPageTrick from './ThePuppetShow/PuppetTricks/AppLandingPageTrick';
 import FancyCategoryTrick from './ThePuppetShow/PuppetTricks/FancyCategoryTrick';
 // import SitemapTrick from './ThePuppetShow/PuppetTricks/SitemapTrick';
 // import { WatchConfig } from './TheWatcher/BaseWatcher';
 
 async function scrape() {
-    const { page, browser } = await initPuppet(defaultLaunchOptions);
+    const { page, browser } = await initPuppet(debugLaunchOptions);
     try {
         const watcher = new ConsoleWatcher({ level: 'info' });
         const puppetMaster = new PuppetMaster(
@@ -23,6 +26,8 @@ async function scrape() {
             "selling-products", puppetMaster, {}, watcher
         )
         await fancyCateTrick.accessPage()
+        const results = await fancyCateTrick.extractDerive()
+        console.log(results);
     } catch (error) {
         console.log(error);
     } finally {
