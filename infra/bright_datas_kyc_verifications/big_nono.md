88fa5f631707fa3f45a15b9e84708fb280c9b05a
Huy Nguyen Vu <huyk25sinh@gmail.com>
2023-12-09 17:54:38 +0700

    update: infrastructure code

---
+++ infra/bright_datas_kyc_verifications/big_nono.md
@@ -0,0 +1,17 @@
+- DDoS attacks
+- Spam: mass posting any mail/message boards/irc/comments messages and images
+- Ad fraud or click fraud
+- Impersonation or spoofing for fraudulent purposes
+- Creating fake accounts or content
+- Use of automatic ticket purchasing (ticket-bots)
+- Raffles, lottery, or gambling
+- Fake engagement (likes, comments, shares, etc.)
+- Collection of nonpublic information (i.e: data behind login)
+- Gaming or trading of in games items/currency
+- Reselling of proxies without Bright Data's prior written approval (see our partners programs)
+- Use of streaming related domains (videos, music or images streaming)
+- Trading crypto/virtual currency or NFT
+- Any Search Engine Optimization (SEO) manipulation (i.e: fake clicks in search engines results)
+- Posting on classified, used, and resell sites (such as Craig's list)
+- Completion of surveys in return for a benefit or anything of value
+- Any form of activity that is in violation of applicable law or regulations or any third party rights
--- scrape/scraper_ts/src/index.ts
+++ scrape/scraper_ts/src/index.ts
@@ -6,7 +6,10 @@ import { defaultLaunchOptions } from "./TheSalesman/config/browser";
 import { ConsoleWatcher } from "./TheWatcher";
 import SitemapTrick from "./ThePuppetShow/PuppetTricks/SitemapTrick";
 import ScrapeResult from "./TheSalesman/ScrapeResult";
-import { ShopifyAppCategory, ShopifyPartner } from "./TheSalesman/ScrapedTable";
+import { HttpUrl, ShopifyAppCategory, ShopifyAppDetail, ShopifyPartner } from "./TheSalesman/ScrapedTable";
+import AppLandingPageTrick from "./ThePuppetShow/PuppetTricks/AppLandingPageTrick";
+import { isEmpty } from "lodash";
+import { Browser, Page } from "puppeteer";
 require('dotenv').config();
 
 export const supabase = createClient<Database>(
@@ -14,93 +17,69 @@ export const supabase = createClient<Database>(
     process.env.SUPABASE_SERVICE_KEY as string
 
 )
-async function getSitemapScrapedResult(): Promise<ScrapeResult>{
+
+async function getOldPartnerId(): Promise<Map<HttpUrl, number>>{
+    const {data, error} = await supabase.from("shopify_partner").select("*")
+    if (error){console.log(error);
+    }
+    let partnersIdx: Map<string, number> = Object();
+    data?.forEach(record => {
+        partnersIdx.set(record.shopify_page, record.id)
+    })
+    return partnersIdx;
+}
+
+async function getSitemapScrapedResult(page: Page, browser: Browser): Promise<ScrapeResult>{
+    const partnerdIdx: Map<HttpUrl, number> = await getOldPartnerId();
+    const watcher = new ConsoleWatcher({level:"info"})
+    const complexMaster = new ComplexMaster(
+    page,browser, { logNullElement: true }, watcher)
+    const sitemapTrick = new SitemapTrick(complexMaster, {}, watcher);
+    const result = await sitemapTrick.scrape();
+    if (result.shopifyAppDetail){
+        result.shopifyAppDetail = result.shopifyAppDetail.map(
+            appDetail => {
+                appDetail.partnerId = partnerdIdx.get(appDetail.partnerId as string)??appDetail.partnerId;
+                return appDetail
+            }
+        )
+    }
+    return result
+}
+async function main(){
     const {page, browser} = await initPuppet(defaultLaunchOptions);
     try{
-        const watcher = new ConsoleWatcher({level:"info"})
-        const complexMaster = new ComplexMaster(
-        page,browser, { logNullElement: true }, watcher)
-        const sitemapTrick = new SitemapTrick(complexMaster, {}, watcher);
-        return await sitemapTrick.scrape();
+        const listAppDetails = (await getSitemapScrapedResult(page,browser)).shopifyAppCategory
+        if (listAppDetails){
+            const appCount = listAppDetails.length;
+            for (const [idx, appDetail] of listAppDetails.entries()){
+                console.log(`Scraping ${idx}/${appCount}`);
+                const watcher = new ConsoleWatcher({level:"info"})
+                const complexMaster = new ComplexMaster(
+                page,browser, { logNullElement: true }, watcher)
+                const appLPTrick = new AppLandingPageTrick(
+                    appDetail.shopifyPage?.split("/")[0] as string,
+                    complexMaster, {}, watcher
+                )
+                const scrapeResult = await appLPTrick.scrape();
+                if (scrapeResult.shopifyAppDetail){
+                    const scrapedApp = scrapeResult.shopifyAppDetail[0];
+                    await supabase.from('shopify_app_info').insert(
+                        {
+                            'app_name': scrapedApp.appName??"",
+                            'avg_rating': scrapedApp.avgRating,
+                            'partner_id': scrapedApp.partnerId,
+                            'shopify_page': scrapedApp.shopifyPage,
+                        })
+                }
+            }
+        }else{
+            console.log("Empty listAppDetails");
+            
+        }
     }finally{
         await browser.close()
     }
-}
-
-async function main(){
-    const {
-        shopifyAppCategory,
-        shopifyPartner
-    } = await getSitemapScrapedResult()
-    
-    const formatedPartner: {
-        shopify_page: string;
-        name: string;
-    }[] = Array.isArray(shopifyPartner)
-        ? shopifyPartner.map((partner: ShopifyPartner): {
-            'shopify_page': string;
-            'name': string;
-          } => ({
-            'shopify_page': partner.shopifyPage ?? "",
-            'name': partner.name ?? ""
-          }))
-        : [];
-    
-    const formatedAppCategories: {
-        shopify_page: string;
-        name: string;
-    }[] = Array.isArray(shopifyAppCategory)
-        ? shopifyAppCategory.map((partner: ShopifyPartner): {
-            'shopify_page': string;
-            'name': string;
-          } => ({
-            'shopify_page': partner.shopifyPage ?? "",
-            'name': partner.name ?? ""
-          }))
-        : [];
-    
-
-    console.log('Uploading to shopifyPartner' + shopifyPartner?.length);
-    const { data: data_shopify_partner, error: error_shopify_partner } = await supabase
-        .from('shopify_partner')
-        .insert(formatedPartner);
-    if (data_shopify_partner) {
-        console.log(data_shopify_partner);
-    }
-    if (error_shopify_partner){
-        console.log(error_shopify_partner);
-    }
-
-    console.log('Uploading to shopifyPartner' + shopifyAppCategory?.length);
-    let { data: data_category, error: error_category } = await supabase
-        .from('shopify_app_category')
-        .insert(formatedAppCategories);
-    if (data_category) {
-        console.log(data_category);
-    }
-    if (error_category){
-        console.log(error_category);
-        
-    }
-    // await Promise.all(shopifyPartner??[].map(
-    //     async(partner:ShopifyPartner) => {
-    //         await supabase.from('shopify_partner').insert(
-    //             {
-    //                 'shopify_page' : partner.shopifyPage??"",
-    //                 'name':partner.name??""
-    //             }
-    //         )
-    // }))
-    // console.log('Uploading to shopifyAppCategory' + shopifyAppCategory?.length);
-    // await Promise.all(shopifyAppCategory??[].map(
-    //     async (cate:ShopifyAppCategory) => {
-    //         await supabase.from('shopify_app_category').insert(
-    //             {
-    //                 "shopify_page": cate.shopifyPage??"",
-    //                 "name": cate.name??""
-    //             }
-    //         )
-    // }))
 }
 
 main()
