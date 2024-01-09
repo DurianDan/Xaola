b5ce8ffde4ffb72f903306397da9c5dffb404bb9
Huy Nguyen Vu <huyk25sinh@gmail.com>
2024-01-09 23:47:18 +0700

    Update: identify types of CategoryXPathConfig type; `elements.ts` > change all xpath selector into css selector. Todo: rename all XPATH objects into CSS objects config; Make `ComplexMaster` get only css instead of xpath;  test all Tricks after `ComplexMaster` has been modified.

--- scrape/scraper_crawlee/src/ThePuppetShow/Acts/identifyAppsCategoryType.ts
+++ scrape/scraper_crawlee/src/ThePuppetShow/Acts/identifyAppsCategoryType.ts
@@ -1,4 +1,29 @@
 import { CheerioAPI } from "cheerio";
 import NoobMaster from "../PuppetMaster/NoobMaster";
+import {
+  shopifyStaticCategoryElements,
+  shopifyFancyStaticCategoryElements,
+  fancyCategoryElements,
+  CategoryXpathPageConfig
+} from "../../TheSalesman/config/elements";
 
 /** @todo: implement indicatting category types*/
+
+const xpathConfigs:CategoryXpathPageConfig[]= [
+    shopifyStaticCategoryElements,
+    shopifyFancyStaticCategoryElements,
+    fancyCategoryElements,
+]
+
+function identifyCategoryXpathConfig(
+loadedHtml: CheerioAPI
+): CategoryXpathPageConfig|null{
+    for (const config of xpathConfigs){
+        if (loadedHtml(config.identifierElement)){
+            return config
+        }
+    }
+    return null
+}
+
+export default identifyCategoryXpathConfig
